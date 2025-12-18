264bdf353241da23acde8656edee5baaaf2a41e9
Georg Mansky-Kummert <georg.mansky-kummert@fh-campuswien.ac.at>
2025-12-18 20:27:11 +0100

    Re-add constants.

--- part10/constants.py
+++ part10/constants.py
@@ -20,3 +20,7 @@ Notes:
   - Load Shakespeare's sonnets via PoetryDB (with local caching)
   - Print how long loading and each query took
 """
+
+POETRYDB_URL = "https://poetrydb.org/author,title/Shakespeare;Sonnet"
+
+CACHE_FILENAME = "sonnets.json"
